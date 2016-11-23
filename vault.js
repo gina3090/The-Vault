894ecd382af0bab4a7daa357498e0358a174afbb
ginagala3090 <ginagala3090@users.noreply.github.com>
2016-11-22 22:33:13 -1000

    created setValue and getValue functions

--- vault.js
+++ vault.js
@@ -1,4 +1,23 @@
 'use strict';
 module.exports = function() {
-  
+
+var vault = {};
+
+  function setValue(key, value){
+    vault.key = value;
+  }
+
+  function getValue(key){
+    if(vault.hasOwnProperty('key')){
+      return vault.key;
+    } else {
+      return null;
+    }
+  }
+
+return {
+  setValue: setValue,
+  getValue: getValue
+};
+
 };
